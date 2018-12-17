e565998c9ef28bf244694508048f7c3ed7736f0a
haruyuki <jumpy12359@gmail.com>
2018-12-17 13:18:56 +1100

    Add missing file

---
+++ src/main/java/moe/haruyuki/cookietools/items/tools/ToolHoe.java
@@ -0,0 +1,27 @@
+package moe.haruyuki.cookietools.items.tools;
+
+import moe.haruyuki.cookietools.CookieTools;
+import moe.haruyuki.cookietools.init.ModItems;
+import moe.haruyuki.cookietools.util.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemHoe;
+
+public class ToolHoe extends ItemHoe implements IHasModel {
+
+    public ToolHoe(String name, Item.ToolMaterial material) {
+
+        super(material);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.TOOLS);
+
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+
+        CookieTools.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
